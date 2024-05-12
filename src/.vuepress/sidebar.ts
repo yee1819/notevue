476d34ec6f5f6fef63590894f124c319a49630f5
yee1819 <103957720+yee1819@users.noreply.github.com>
2024-05-12 21:07:01 +0800

    Update sidebar.ts

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -4,7 +4,7 @@ export default sidebar({
   "/": [
     "",
     {
-      text: "案例",
+      text: "C++",
       icon: "laptop-code",
       prefix: "demo/",
       link: "demo/",
@@ -15,11 +15,6 @@ export default sidebar({
       icon: "book",
       prefix: "guide/",
       children: "structure",
-    },
-    {
-      text: "幻灯片",
-      icon: "person-chalkboard",
-      link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
     },
   ],
 });
