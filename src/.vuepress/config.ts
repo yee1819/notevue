e7cc90cebc45e55887576f5fee92bf66470f809f
yee1819 <103957720+yee1819@users.noreply.github.com>
2024-05-12 20:47:18 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/notevue/",
+  base: "/",
 
   lang: "zh-CN",
   title: "Kirari的笔记本",
